c5d8d9dea6d4cebe399fb01aa63556aaad25dd28
JonahTuckman <jtuckman9@gmail.com>
2019-03-18 17:03:39 -0400

    East side

--- firstround.py
+++ firstround.py
@@ -1,25 +1,76 @@
 import random
 from numpy.random import choice
 
+
+## Global Probabilities
+prob16 = [(135/136), (1/136)]
+prob8 = [.50, .50]
+prob12 = [(89/136), (47/136)]
+prob13 = [.79, .21]
+prob11 = [.625, .375]
+prob14 = [(115/136), (21/136)]
+prob10 = [(84/136), (52/136)]
+prob15 = [(128/136), (8/136)]
+
+
+## Will use choices when this is 1
+Ready = ['1' , '0']
+probsReady = [.2, .8]
+drawReady = choice(Ready, 1, True, probsReady)
+print(drawReady)
+
+######################################
+############# EAST SIDE ##############
+######################################
+
+
 ## 16 vs 1
 # No. 1 seeds are 135-1 against No. 16 seeds
-seeds16 = ['1', '16']
-prob16 = [(135/136), (1/136)]
-draw16 = choice(seeds16, 1, True, prob16 )
-print(draw16)
+seeds16E = ['Duke', 'NDS']
+draw16E = choice(seeds16E, 1, True, prob16 )
+print(draw16E)
 
 
 # 8 vs 9
 # 50/50 proposition, as No. 8 and No. 9 seeds have split their 136 meetings since 1985
-seeds8 = ['8', '9']
-prob8 = [.50, .50]
-draw8 = choice(seeds8, 1, True, prob8)
-print(draw8)
+seeds8E = ['VCU', 'UCF']
+draw8E = choice(seeds8E, 1, True, prob8)
+print(draw8E)
 
 # 12 vs 5
 # 47 upsets since 1985. That is 136 games
-seeds12 = ['5', '12']
-prob12 = [(89/136), (47/136)]
-draw12 = choice(seeds12, 1, True, prob12)
-print(draw12)
+seeds12E = ['Mississippi State', 'Liberty']
+draw12E = choice(seeds12E, 1, True, prob12)
+print(draw12E)
+
+# 13 vs 4
+## upset 21 percent of the time
+seeds13E = ['Virginia Tech', 'Saint Louis']
+draw13E = choice(seeds13E, 1, True, prob13)
+print(draw13E)
+
+## 11 vs 6
+## 51 out of 136 opening round match-ups, or 37.5 percent of the time.
+seeds11E = ['Maryland', 'Belmont']
+draw11E = choice(seeds11E, 1, True, prob11)
+print(draw11E)
+
+# 14 vs 3
+## 21/136 all time
+seeds14E = ['LSU', 'Yale']
+draw14E = choice(seeds14E, 1, True, prob14)
+print(draw14E)
+
+## 10 vs 7
+## 10 seeds are 52-84 against 7 seeds
+seeds10E = ['Louisville', 'Minnesota']
+draw10E = choice(seeds10E, 1, True, prob10)
+print(draw10E)
+
+## 15 vs 2
+## 8-128 all time
+seeds15E = ['Michigan State', 'Bradley']
+draw15E = choice(seeds15E, 1, True, prob15)
+print(draw15E)
+
 
