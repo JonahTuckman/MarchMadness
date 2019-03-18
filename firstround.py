3b8fe26134ef36f0f869f6e9b74d6225b2cdc044
JonahTuckman <jtuckman9@gmail.com>
2019-03-18 17:08:59 -0400

    South Side

--- firstround.py
+++ firstround.py
@@ -19,6 +19,9 @@ probsReady = [.2, .8]
 drawReady = choice(Ready, 1, True, probsReady)
 print(drawReady)
 
+print(" ")
+print(" ")
+
 ######################################
 ############# EAST SIDE ##############
 ######################################
@@ -74,3 +77,64 @@ draw15E = choice(seeds15E, 1, True, prob15)
 print(draw15E)
 
 
+
+print(" ")
+print(" ")
+
+######################################
+############# SOUTH SIDE #############
+######################################
+
+
+## 16 vs 1
+# No. 1 seeds are 135-1 against No. 16 seeds
+seeds16S = ['Virginia', 'Gardner-Webb']
+draw16S = choice(seeds16S, 1, True, prob16 )
+print(draw16S)
+
+
+# 8 vs 9
+# 50/50 proposition, as No. 8 and No. 9 seeds have split their 136 meetings since 1985
+seeds8S = ['Ole Miss', 'Oklahoma']
+draw8S = choice(seeds8S, 1, True, prob8)
+print(draw8S)
+
+# 12 vs 5
+# 47 upsets since 1985. That is 136 games
+seeds12S = ['Wisconsin', 'Oregon']
+draw12S = choice(seeds12S, 1, True, prob12)
+print(draw12S)
+
+# 13 vs 4
+## upset 21 percent of the time
+seeds13S = ['Kansas State', 'Uc Irvine']
+draw13S = choice(seeds13S, 1, True, prob13)
+print(draw13S)
+
+## 11 vs 6
+## 51 out of 136 opening round match-ups, or 37.5 percent of the time.
+seeds11S = ['Villanova', 'Saint Marys']
+draw11S = choice(seeds11S, 1, True, prob11)
+print(draw11S)
+
+# 14 vs 3
+## 21/136 all time
+seeds14S = ['Purdue', 'Old Dominion']
+draw14S = choice(seeds14S, 1, True, prob14)
+print(draw14S)
+
+## 10 vs 7
+## 10 seeds are 52-84 against 7 seeds
+seeds10S = ['Cincinnati', 'Iowa']
+draw10S = choice(seeds10S, 1, True, prob10)
+print(draw10S)
+
+## 15 vs 2
+## 8-128 all time
+seeds15S = ['Tennessee', 'Colgate']
+draw15S = choice(seeds15S, 1, True, prob15)
+print(draw15S)
+
+
+print(" ")
+print(" ")
